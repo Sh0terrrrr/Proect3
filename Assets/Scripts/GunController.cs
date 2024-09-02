f542c91b415183a901a954283813dc043822b57b
Sh0terrrrr <starkovelisey2000@gmail.com>
2024-09-02 22:48:24 +0300

    Initial commit

---
+++ Assets/Scripts/GunController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour
+{
+    private float horizontal;
+    private float vertical;
+    public float sensitivity = 3;
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxis("Mouse X") * sensitivity;
+        transform.Rotate(0, horizontal, 0);
+        vertical = Input.GetAxis("Mouse Y") * sensitivity;
+        transform .Rotate(0, 0, vertical);
+    }
+}
